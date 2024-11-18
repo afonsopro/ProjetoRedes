525cb07a453ee4838c2264c9d90dc5bd6a6af456
Hitlerrrrrrr <goncalorodtigues@gmail.com>
2024-11-18 09:21:52 +0000

    Update menu.php

--- menu.php
+++ menu.php
@@ -11,22 +11,28 @@
         <a href="#" class="logo" style="font-size: 32px; font-weight: bold; font-style: italic; text-decoration: none; color: #ffffff;">
             <span class="leal" style="color: #ffffff;">Leal</span><span style="color: #f39c12;">Cars</span>
         </a>
-        <div style="flex-grow: 1; display: flex; justify-content: center; padding-left: 20px;"> <!-- Ajustei o padding-left para mover para a esquerda -->
+        <div style="flex-grow: 1; display: flex; justify-content: center; padding-left: 20px;">
             <div style="display: flex; align-items: center; position: relative;">
-                <!-- Aumentei o padding em cima e embaixo e alterei a altura da barra de pesquisa -->
                 <input type="text" placeholder="O que estás à procura?" style="padding: 12px 30px; border-radius: 20px; border: 1px solid #ccc; outline: none; width: 400px; height: 18px; font-family: 'Poppins'; font-size: 15px;">
                 <img src="search.png" alt="Search" style="position: absolute; right: 10px; width: 20px; height: 20px; cursor: pointer;">
             </div>
         </div>
         <ul style="display: flex; gap: 20px; list-style-type: none; padding: 0; margin: 0; margin-right: 40px;">
-            <li style="font-size: 15px;">
+            <li style="font-size: 14px;">
                 <a href="#sobre-nos" style="color: #ffffff; text-decoration: none;">Sobre Nós</a>
             </li>
-            <li style="font-size: 15px;">
+            <li style="font-size: 14px;">
                 <a href="#contactos" style="color: #ffffff; text-decoration: none;">Contactos</a>
             </li>
         </ul>
-        <div style="display: flex; align-items: center; gap: 20px; margin-left: 20px; transform: translateY(2px);"> <!-- Ajustei a posição das imagens para baixo -->
+        <a href="#venda-carro" style="text-decoration: none;">
+            <button style="background-color: #f39c12; color: #ffffff; border: 1px solid #303030; padding: 10px 20px; border-radius: 5px; font-size: 14px; cursor: pointer; font-family: 'Poppins';">
+                Venda o seu carro
+            </button>
+        </a>
+        <!-- Barra vertical para separar -->
+        <span style="font-size: 30px; margin-left: 20px; margin-right: 20px; color: #ffffff;">|</span>
+        <div style="display: flex; align-items: center; gap: 20px; transform: translateY(2px);">
             <a href="#signup-page" style="text-decoration: none;">
                 <img src="signup1.jpg" alt="Signup" style="width: 30px; height: 30px;">
             </a>
