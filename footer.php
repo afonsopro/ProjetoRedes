49a3c9624b27e75ec307020f73ec128ad7c6d82b
afonsopro <afonso007costa@gmail.com>
2024-11-28 08:10:30 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer id="footer"; style="background-color: #f39c12; color: white; text-align: center; padding: 10px; position: sticky; bottom: 0; width: 98.6%;">
+    <p>Contactos: +351 961 234 567 | Email: Lealcars@gmail.com</p>        
+    <p>Av. Pedro Nunes Nº1, Rio de Mouro, Sintra, Lisboa, Portugal</p>        
+    <p>&copy; 2024 LealCars. Todos os direitos reservados.</p>
+</footer>
