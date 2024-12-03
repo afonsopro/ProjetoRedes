5b8f50ff9b71efa097fdc400927787ac0bfb2f30
afonsopro <afonso007costa@gmail.com>
2024-12-03 08:55:24 +0000

    Update lealcars.sql

--- BaseDeDados/lealcars.sql
+++ BaseDeDados/lealcars.sql
@@ -7,6 +7,9 @@
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
+CREATE DATABASE lealcars;
+USE lealcars;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
